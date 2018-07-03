b038727e211455f61baae5848b5a2adb5a32769f
sarwansingh <sarwan_singh@yahoo.com>
2018-07-03 16:46:02 +0530

    update

--- ST402/6.LCD-Keypad/securitySys/securitySys.ino
+++ ST402/6.LCD-Keypad/securitySys/securitySys.ino
@@ -7,7 +7,7 @@ int buzz=12;
 
 void setup() {
  lcd.begin(16,2);
- lcd.print("Welcome LCD-Keypad");
+ lcd.print("Enter pass");
  // all rows OUTPUT and cols INPUT
  pinMode(R1,OUTPUT);pinMode(R2,OUTPUT);pinMode(R3,OUTPUT);pinMode(R4,OUTPUT);
  pinMode(C1,INPUT);pinMode(C2,INPUT);pinMode(C3,INPUT);
@@ -17,40 +17,21 @@ void setup() {
 }
 void buzzON(){digitalWrite(buzz,HIGH);}
 void buzzOFF(){digitalWrite(buzz,LOW);}
-int row=1,col=0;
+ int n=0,pass=321;
 void loop() {
   digitalWrite(R1,LOW);digitalWrite(R2,HIGH);
   digitalWrite(R3,HIGH);digitalWrite(R4,HIGH);
-  while (digitalRead(C1) ==  LOW) {
-      lcd.setCursor(--col,row); 
-      lcd.print("1"); buzzON();delay(300);buzzOFF();col++;
-      
-    if  (digitalRead(C1) ==  LOW){
-      lcd.setCursor(--col,row); 
-      lcd.print("A"); delay(300);col++;
-    }
-    if  (digitalRead(C1) ==  LOW){
-      lcd.setCursor(--col,row); 
-      lcd.print("B"); delay(300);col++;
-    }
-    //if  (digitalRead(C1) ==  LOW)lcd.print("B"); delay(300);
+  if  (digitalRead(C1) ==  LOW) {     
+      lcd.print("*"); buzzON();delay(300);buzzOFF();
+      n=n*10+1;
   }
   if (digitalRead(C2) ==  LOW) {
-      lcd.setCursor(col,row); 
-      lcd.print("2"); buzzON();delay(300);buzzOFF();
-      col++;
-    if  (digitalRead(C2) ==  LOW){
-      lcd.setCursor(--col,row); 
-      lcd.print("D"); delay(300);col++;
-    }
-    if  (digitalRead(C2) ==  LOW){
-      lcd.setCursor(--col,row); 
-      lcd.print("E"); delay(300);col++;
-    }
+      lcd.print("*"); buzzON();delay(300);buzzOFF();
+      n=n*10+2;
    }
-  if (digitalRead(C3) ==  LOW) {
-      lcd.setCursor(col,row); 
-      lcd.print("3"); buzzON();delay(300);buzzOFF();col++;
+  if (digitalRead(C3) ==  LOW) {  
+      lcd.print("*"); buzzON();delay(300);buzzOFF();
+      n=n*10+3;
    }
  //////////// second row
  digitalWrite(R1,HIGH);digitalWrite(R2,LOW);
@@ -61,7 +42,10 @@ void loop() {
   //////////// third row
  digitalWrite(R1,HIGH);digitalWrite(R2,HIGH);
   digitalWrite(R3,LOW);digitalWrite(R4,HIGH);
-  if (digitalRead(C1) ==  LOW) {lcd.print("7"); delay(300);}
+  if (digitalRead(C1) ==  LOW) {
+      lcd.print("+"); 
+      n=0; 
+      delay(300);}
   if (digitalRead(C2) ==  LOW) {lcd.print("8"); delay(300);}
   if (digitalRead(C3) ==  LOW) {lcd.print("9"); delay(300);} 
    //////////// fourth row
@@ -69,6 +53,11 @@ void loop() {
   digitalWrite(R3,HIGH);digitalWrite(R4,LOW);
   if (digitalRead(C1) ==  LOW) {lcd.clear(); lcd.setCursor(0,0);}
   if (digitalRead(C2) ==  LOW) {lcd.print("0"); delay(300);}
-  if (digitalRead(C3) ==  LOW) {lcd.print("#"); delay(300);} 
+  if (digitalRead(C3) ==  LOW) {lcd.print("="); 
+      if (n==pass){ lcd.print("OK");}
+      else { lcd.print(" not OK"); } 
+      delay(300);
+      lcd.setCursor(0,1);
+  }    
   
 }
