3d499b1773323db460e6bab31d6e279cc5526a72
sarwansingh <sarwan_singh@yahoo.com>
2019-02-22 12:07:25 +0530

    Update DS1307.ino

--- ST392/7a.DS1307/DS1307.ino
+++ ST392/7a.DS1307/DS1307.ino
@@ -22,7 +22,7 @@ void setup () {
     // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-//     rtc.adjust(DateTime(2018,6, 12, 9, 7,20));
+   // rtc.adjust(DateTime(2019,2, 22, 10, 19,00));
   }
 }
 
