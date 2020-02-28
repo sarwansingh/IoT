b2477c5b189330972ebfa107bd90e0f79f73a8da
sarwansingh <sarwan_singh@yahoo.com>
2020-02-28 12:46:54 +0530

    update

---
+++ ClassExamples/PJ125/NodeMCU/LED_button/LED_button.ino
@@ -0,0 +1,27 @@
+const int buttonPin = 0;     // onboard  flash button
+const int ledPin =  2;      // onboard LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  if ( digitalRead(buttonPin) ==HIGH) {
+      if (buttonState ==1) buttonState =0;
+      else buttonState =1;
+      while(digitalRead(buttonPin) ==HIGH ) ;
+  }
+  
+
+  if (buttonState == 1) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
