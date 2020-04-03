a5e9c9243cd80a77935b2a67e2717ef3875bad64
sarwansingh <sarwan_singh@yahoo.com>
2020-04-03 09:51:11 +0530

    update

--- ClassExamples/PJ125/Firebase/FirebaseDHT_NodeMCU_v2/FirebaseDHT_NodeMCU_v2.ino
+++ ClassExamples/PJ125/Firebase/FirebaseDHT_NodeMCU_v2/FirebaseDHT_NodeMCU_v2.ino
@@ -7,18 +7,16 @@
 #include  <ESP8266WiFi.h>
 
 // Set these to run example.
-#define FIREBASE_HOST "nielit-firebase125.firebaseio.com"  // the project name address from firebase id
-#define FIREBASE_AUTH "t8wGji1XdcQc8s6Gb9g4uOaE0lnjlbEp7ZmtFPtN"  // the secret key generated from firebase
-#define WIFI_SSID "Exodus_07"                  // wifi name 
-#define WIFI_PASSWORD "robin241095"                 //password of wifi 
+#define FIREBASE_HOST "nodemcu-nielit125.firebaseio.com"
+#define FIREBASE_AUTH "2QVppV5F95FDPGTp39AlD29BUAQUlOk2tBTnopjn"
+#define WIFI_SSID     "demo123"
+#define WIFI_PASSWORD "12345678"
 
 //
 //#define FIREBASE_HOST "yyyyyyyyyyyy.firebaseio.com"
 //#define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-//#define WIFI_SSID   "ssssss"
+//#define WIFI_SSID     "ssssss"
 //#define WIFI_PASSWORD "pppppp"
-
-#define MEASURES "measures" // Firebase Realtime Database node to store measures
 
 #define DHTPIN 14    // Data Pin of DHT 11 , for NodeMCU D5 GPIO no. is 14
 
@@ -39,33 +37,34 @@ void setup() {
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
- 
+ float h=0 ,t=-1;
 void loop() {
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();  // Reading temperature as Celsius (the default)
-
-  Firebase.pushInt(MEASURES, t);
+  h = dht.readHumidity();
+  t = dht.readTemperature();  // Reading temperature as Celsius (the default)
+//  h++ ; t++; // testing
+  Serial.print("Humidity: ");  Serial.print(h);
+  Serial.print("%  Temperature: ");  Serial.print(t);  Serial.println("°C ");
   
-//  Firebase.setFloat ("Temp", t);
-//  Firebase.setFloat ("Humidity", h);
+  String name = Firebase.pushInt("measures", t);
+  //String name1 = Firebase.pushInt("measures", h);
+  
+  Firebase.setFloat ("Temp", t);
+  Firebase.setFloat ("Humidity", h);
   // handle error
   if (Firebase.failed()) {
       Serial.print("pushing failed : ");
       Serial.println(Firebase.error());  
       return;
+  }else {
+    Serial.print("pushed: /measures/");
+    Serial.println(name);
   }
-  
-  Serial.print ("Temp :");
-  Serial.println(t);
-  Serial.print ("Humidity :");
-  Serial.println(h);
    
-  delay(30000);  // 30 second wait
+  delay(20000);  // 10 second wait
 
 // for realtime implementation
 // Now let's go in deep sleep for 5 minutes:
 // ESP.deepSleep(300e6, WAKE_RF_DEFAULT); // 1st parameter is in µs!
-  
 }
  
